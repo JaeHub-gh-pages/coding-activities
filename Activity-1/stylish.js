0d0547f853d96a412488ba5996045e1f6aa11d2a
The Supreme Jae <Jaekim06789@gmail.com>
2018-07-09 11:07:53 -0700

    Finished Rock Paper Scissor Game
    Activity 2 :)

---
+++ Activity-1/stylish.js
@@ -0,0 +1,67 @@
+
+var min = 0;
+var max = 255;
+
+
+function setbackground()
+{
+    window.setTimeout( "setbackground()", 80); //  milliseconds delay
+
+    var index = Math.round(Math.random() * 9);
+
+    var ColorValue = "FFFFFF"; // default color - white (index = 0)
+
+    if(index == 1)
+        ColorValue = "66FF33"; 
+    if(index == 2)
+        ColorValue = "FF0000"; 
+    if(index == 3)
+        ColorValue = "FF00FF"; 
+    if(index == 4)
+        ColorValue = "0000FF"; 
+    if(index == 5)
+        ColorValue = "00FFFF"; 
+    if(index == 6)
+        ColorValue = "FFFF00"; 
+    if(index == 7)
+       ColorValue = "CC66FF"; 
+    if(index == 8)
+        ColorValue = "3366FF"; 
+   if(index == 9)
+        ColorValue = "CCCCCC"; 
+
+   document.getElementsByTagName("body")[0].style.backgroundColor = "#" + ColorValue;
+
+}
+
+function setbackgroundTitle()
+{
+    window.setTimeout( "setbackgroundTitle()", 600); //  milliseconds delay
+
+    var index = Math.round(Math.random() * 4);
+
+    var ColorValue = "FFFFFF"; // default color - white (index = 0)
+
+    if(index == 1)
+        ColorValue = "66FF33"; 
+    if(index == 2)
+        ColorValue = "FF0000"; 
+    if(index == 3)
+        ColorValue = "FF00FF"; 
+    if(index == 4)
+        ColorValue = "0000FF"; 
+
+
+    document.getElementById("title")[0].style.backgroundColor = "#" + ColorValue;
+
+}
+
+function sparkle() {
+    var x = document.getElementById("sparkles");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+        
+    }
+}
